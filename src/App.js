67e173d98c1d50754642979cd58d4d7222b097dc
Mandi Haase <haase1020@gmail.com>
2020-03-01 21:29:10 -0500

    add fooder component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 
 import './App.css';
 import SignIn from './components/SignIn';
+import StickyFooter from './components/StickyFooter';
 
 function App() {
   return (
     <div className="App">
       <SignIn />
+      <StickyFooter />
     </div>
   );
 }
